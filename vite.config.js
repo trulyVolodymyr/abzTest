b2a45a17f786f680e1722751686f2232fae21e9f
vladimir vychikov <vovaorc@MacBook-Pro-Vychikov.local>
2022-07-30 23:21:50 +0300

    vite base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/deploying-vite-project-expemple/',
+  base: '/abzTest/',
   plugins: [vue()],
 })
