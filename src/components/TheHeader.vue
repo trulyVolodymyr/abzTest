04c34a5760d8da2ee6a2bc5e298a9575c8c032e5
vladimir vychikov <vovaorc@MacBook-Pro-Vychikov.local>
2022-07-30 20:53:08 +0300

    final

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="header">
+    <div class="header_wrp">
+      <img class="header_logo" src="../assets/logo.svg" alt="" />
+      <div class="header_btns">
+        <a href="#users" class="btn">Users</a>
+        <a href="#post" class="btn">Sign up</a>
+      </div>
+    </div>
+  </div>
+</template>
